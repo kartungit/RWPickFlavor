c7760022770f0898a4e322f6c19a53f2194af7bc
Thinh Mai <thinh.mai@rockship.co>
2018-10-29 22:14:26 +0700

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by Ngoc Tran on 29/10/18.
+//  Copyright © 2018 Thinh Mai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
